a99f35945c166b17fb2f1c274bff251aa403ab6e
ir_fuel <joris@astus.be>
2016-01-02 19:23:58 +0100

    fix

--- DateTime.js
+++ DateTime.js
@@ -320,8 +320,8 @@ var Datetime = React.createClass({
 		return DOM.div({className: className}, children.concat(
 			DOM.div(
 				{ key: 'dt', className: 'rdtPicker',style:{} },
-				[DOM.span({style:{flexGrow:'1.5'},key:'days'},React.createElement( Component, this.getComponentProps())),
-					DOM.span({style:{flexGrow:'1'},key:'time'},React.createElement( this.viewComponents.time, this.getComponentProps()))]
+				[DOM.div({style:{flexGrow:'1.5'},key:'days'},React.createElement( Component, this.getComponentProps())),
+					DOM.div({style:{flexGrow:'1'},key:'time'},React.createElement( this.viewComponents.time, this.getComponentProps()))]
 
 			)
 		));
